f8a1ba3773024571945b0d05f9ae77e316b9e16b
dingc <1109928059@qq.com>
2024-08-12 21:17:33 +0800

    整合swagger-ui

--- src/main/java/com/boluo/Application.java
+++ src/main/java/com/boluo/Application.java
@@ -5,14 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+
+        // SpringApplication.run(Application.class, args);
 
         logger.info("Start Demo Service Application!!");
         SpringApplication springApplication = new SpringApplication(Application.class);
--- src/main/java/com/boluo/controller/HelloController.java
+++ src/main/java/com/boluo/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.boluo.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022/10/29 12:56
  * @desc
  */
-@RestController
+@Api(value = "测试value", tags = "测试tag")
+@RestController // 数据将以json格式返回
 public class HelloController {
 
-    @RequestMapping("/hello")
+    // RequestMapping 会在swagger-ui上产生多个
+    @PostMapping("/hello")
     public String hello() {
         return "hello";
     }
+
+
+    @PostMapping(value = "/kafka/consumerMsg")
+    public ResponseEntity<String> getMsgByKafka() {
+        try {
+            return ResponseEntity.ok().body("测试信息!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
 }
